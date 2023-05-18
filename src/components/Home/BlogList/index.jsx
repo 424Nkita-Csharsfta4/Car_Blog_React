33a2e9382e56071d548f123ef34afe2ff5bbd9af
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-05-18 22:57:26 +0300

    Add files via upload

--- src/components/Home/BlogList/index.jsx
+++ src/components/Home/BlogList/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import BlogItem from "./BlogItem";
 
-
 const BlogList = ({ blogs }) => {
   return (
     <div className="blogList-wrap">
       {blogs.map((blog) => (
-        <BlogItem blog={blog} />
+        <BlogItem key={blog.id} blog={blog} />
       ))}
     </div>
   );
